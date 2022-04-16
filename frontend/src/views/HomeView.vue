7da2ac6392a3cf2d644fdee7f80d6cadcbcc1bac
Grant Henderson <granthend00@gmail.com>
2022-04-16 12:33:25 -0700

    Paring down initial files

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
   </main>
 </template>
