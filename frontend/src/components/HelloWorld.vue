94e500c0ab67d056b7d3df1a1afc1fe6c4000805
Nav <13.navjot.singh@gmail.com>
2022-04-16 13:39:04 -0700

    input text field added

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -10,7 +10,16 @@ defineProps({
 <template>
   <div class="centered">
     <img id="logo centered" src="..\assets\firesight.png">
+    <div class="centered">
+    <span class="p-input-icon-left p-input-icon-right">
+    <i class="pi pi-search" />
+    <InputText type="text" v-model="value3" placeholder="Enter Location" />
+    <i class="pi pi-spin pi-spinner" />
+    </span>
+    </div>
+    <div class="centeredTopPadding">
     <Button label="Primary" class="p-button-outlined centered" />
+    </div>
   </div>
 </template>
 
@@ -24,6 +33,11 @@ h3 {
 
 .centered {
   text-align: center;
+}
+
+.centeredTopPadding {
+    padding-top: 2%;
+    text-align: center;
 }
 
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
 const app = createApp(App)
 
 app.use(PrimeVue);
 app.component('Button', Button)
+app.component('InputText', InputText)
 
 app.use(router)
 
