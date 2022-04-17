39271673fec2d6201f07daf63f6bccbe2062e46d
Grant Henderson <granthend00@gmail.com>
2022-04-17 00:46:57 -0700

    frontend stuff

--- frontend/src/views/DataView.vue
+++ frontend/src/views/DataView.vue
@@ -6,10 +6,10 @@ import Resources from '../components/Resources.vue'
 <template>
   <div>
     <Display v-bind="{lat: $route.params.lat, lng: $route.params.lng }"/>
-    <div class="centeredTopPadding">
-      <Button @click="this.$router.push({ name: 'home'})" label="Return" class="p-button-raised centered" />
-    </div>
     <Resources />
+    <div class="centeredTopPadding">
+      <Button @click="this.$router.push({ name: 'home'})" label="Return to Home" class="p-button-raised centered footer" />
+    </div>
   </div>
 </template>
 
@@ -32,6 +32,13 @@ import Resources from '../components/Resources.vue'
 
 <style scoped>
 
+.footer {
+  position: fixed;
+  left: 50%;
+  bottom: 20px;
+  transform: translate(-50%, -50%);
+  margin: 0 auto;
+}
 .centeredTopPadding {
     padding-top: 2%;
     text-align: center;
